41ac35cff95ad3d900902ddb744ea4c6269b6408
Ryan Castellucci <code@ryanc.org>
2025-01-05 21:26:14 +0000

    working

--- patcher.py
+++ patcher.py
@@ -12,6 +12,10 @@ import struct
 from pathlib import Path
 from hashlib import sha256
 from struct import unpack, unpack_from
+from binascii import hexlify
+
+# local
+from compress import compress
 
 SEEK_SET = 0
 SEEK_CUR = 1
@@ -31,9 +35,18 @@ PART_DATA_SUBTYPE = {
 class ESPImage:
     def __init__(self, data):
         self._data = data
-        self._checksum = 0xEF
+
+        self._checksum = None
+        self._checksum_offset = None
+        self._hash_offset = None
+
         self._code_begin = None
         self._code_end = None
+
+        self._segments_begin = None
+
+        self._dirty = False
+
         self._parse_header()
 
     # wrappers
@@ -41,15 +54,34 @@ class ESPImage:
     def tell(self, *a, **kw): return self._data.tell(*a, **kw)
     def read(self, *a, **kw): return self._data.read(*a, **kw)
 
-    def write(self, b, update_checksum=False, /):
-        if update_checksum:
+    def getvalue(self): return self._data.getvalue()
+    def getbuffer(self): return self._data.getbuffer()
+
+    # write, but it updates the checksum
+    def write(self, b, /):
+        pos = self.tell()
+        if self._checksum is not None and self._code_begin <= pos < self._checksum_offset:
+            # we just compute the checksum difference
             n = len(b)
             a = self.read(n)
             self.seek(-n, SEEK_CUR)
             for x, y in zip(a, b):
                 self._checksum ^= x ^ y
 
+        self._dirty = True
         return self._data.write(b)
+
+    def flush(self):
+        pos = self.tell()
+        self.seek(self._checksum_offset)
+        self.write(bytes([self._checksum]))
+        self.seek(pos)
+        print(f'[+] Updated Checksum:  0x{self._checksum:02X}')
+
+        self._update_hash()
+
+        self._dirty = False
+        return self._data.flush()
 
     def unpack(self, fmt):
         n = struct.calcsize(fmt)
@@ -97,10 +129,8 @@ class ESPImage:
     def _parse_ota(self):
         self._code_begin = self.tell()
 
-        # magic, n_segments, spi_mode, flash_size, entry_point = unpack('<BBBBI', common_header)
         # https://docs.espressif.com/projects/esptool/en/latest/esp8266/advanced-topics/firmware-image-format.html
-        common_header = self.read(8)
-        magic, n_segments, spi_mode, flash_size, entry_point = unpack('<BBBBI', common_header)
+        magic, n_segments, spi_mode, flash_size, entry_point = self.unpack('<BBBBI')
 
         # ESP32 firmware images have a larger header, which we detect with heurstics
         # https://docs.espressif.com/projects/esptool/en/latest/esp32/advanced-topics/firmware-image-format.html
@@ -109,16 +139,50 @@ class ESPImage:
             self.seek(-16, SEEK_CUR)
 
         # walk through the segments
+        self._segments_begin = self.tell()
         for n in range(n_segments):
             self._parse_segment(n)
 
+        self.seek(15 - (self.tell() % 16), SEEK_CUR)
+        self._checksum_offset = self.tell()
+        self._checksum = self.read(1)[0]
+        print(f'[*] Original Checksum: 0x{self._checksum:02X}')
+        self._code_end = self.tell()
+        maybe_hash = self.read(32)
+        if len(maybe_hash) == 32:
+            self.seek(self._code_begin)
+            check_hash = sha256(self.read(self._code_end - self._code_begin)).digest()
+            if check_hash == maybe_hash:
+                self._hash_offset = self.tell() - 32
+                print(f'[*] Original Hash:     {hexlify(maybe_hash).decode()}')
+
     def _parse_segment(self, n):
-        segment_header = self.read(8)
-        mem_offset, segment_size = unpack('<II', segment_header)
-        print(f'[*] segment {n:2} 0x{segment_size:06X}:0x{self.tell():06X} @ 0x{mem_offset:08X}')
+        mem_offset, segment_size = self.unpack('<II')
+        #print(f'[*] Segment {n:2} 0x{segment_size:06X}:0x{self.tell():06X} @ 0x{mem_offset:08X}')
         self.seek(segment_size, SEEK_CUR)
         #segment = data.read(segment_size)
         #for x in segment: self._checksum ^= x
+
+    def _update_hash(self):
+        if self._hash_offset is not None:
+            pos = self.tell()
+            self.seek(self._code_begin)
+            sha = sha256(self.read(self._code_end - self._code_begin)).digest()
+            self.write(sha)
+            self.seek(pos)
+            print(f'[+] Updated Hash:      {hexlify(sha).decode()}')
+
+    def patch_string_at(self, key, string, offset, size):
+        b = string.encode()
+        length = len(b)
+        if length > size:
+            raise ValueError(f'Cannot fit string with encoded size {length} in {size} bytes!')
+
+        print(f'[+] Writing {length} bytes to {key}[{size}] @ {offset}...')
+        # zero pad to size
+        b += b'\0' * (size - len(b) + 1)
+        self.seek(offset)
+        self.write(b)
 
 def _read_as_bytesio(path):
     if path.suffix == '.gz':
@@ -129,17 +193,6 @@ def _read_as_bytesio(path):
         with open(path, 'rb') as f:
             return io.BytesIO(f.read())
 
-def patch_string(f, key, string, offset, size):
-    b = string.encode()
-    length = len(b)
-    if length > size:
-        raise ValueError(f'Cannot fit string with encoded size {length} in {size} bytes!')
-
-    print(f'[*] Writing {length} bytes to {key}[{size}] @ {offset}...')
-    b += b'\0' * (size - len(b) + 1)
-    f.seek(offset)
-    f.write(b)
-
 def patch_binary(source, target, to_patch):
     if len(to_patch) == 0:
         return
@@ -147,72 +200,28 @@ def patch_binary(source, target, to_patch):
     target_path = Path(target).absolute()
     source_path = Path(source).absolute()
 
-    print(f'Patching `{source}` -> `{target}`...')
-    with open(source_path, 'rb') as f:
-        raw = f.read()
-        if source_path.suffix == '.gz':
-            data = io.BytesIO(gzip.decompress(raw))
-        else:
-            data = io.BytesIO(raw)
+    print(f'[+] Patching `{source_path.name}` -> `{target_path.name}`...')
 
-    data.seek(-33, SEEK_END)
-    checksum = data.read(1)[0]
-    print(f'[*] Initial checksum 0x{checksum:02X}')
+    data = ESPImage(_read_as_bytesio(source_path))
 
     for m in re.finditer(rb'PLACEHOLDER_FOR_(\w+)\s+\0', data.getvalue()):
         key = m.group(1).decode()
         offset, end = m.span()
         size = (end - offset) - 1
         if (value := to_patch.get(key, None)) is not None:
-            b = value.encode()
-            length = len(b)
-            if length > size:
-                raise ValueError(f'Cannot fit string with encoded size {length} in {size} bytes!')
-
-            print(f'[*] Writing to {key}[{size}] @ {offset}...')
-            b += b'\0' * (size - len(b))
-            data.seek(offset)
-            replaced = data.read(size)
-            data.seek(offset)
-            for x, y in zip(b, replaced):
-                checksum ^= x ^ y
-            data.write(b)
+            data.patch_string_at(key, value, offset, size)
 
     data.flush()
-    if '.factory.' not in str(target_path):
-        data.seek(0)
-
-        chip_name = 'esp8266'
-        common_header = data.getvalue()[0:8]
-        magic = common_header[0]
-        if magic in (ESPLoader.ESP_IMAGE_MAGIC, ESP8266V2FirmwareImage.IMAGE_V2_MAGIC):
-            extended_header = data.getvalue()[8:24]
-            #print(extended_header)
-            chip_id = int.from_bytes(extended_header[4:5], "little")
-            for rom in [n for n in ROM_LIST if n.CHIP_NAME != "ESP8266"]:
-                if chip_id == rom.IMAGE_CHIP_ID:
-                    chip_name = rom.CHIP_NAME
-                    break
-
-        print(f'[*] Adjusting checksum to 0x{checksum:02X}...')
-        data.seek(-33, SEEK_END)
-        data.write(bytes([checksum]))
-
-        sha = sha256(data.getvalue()[:-32]).digest()
-        # 32 bytes to the end
-        data.seek(-32, SEEK_END)
-        data.write(sha)
-        data.flush()
 
     if target_path.suffix == '.gz':
-        print('[*] Compressing...')
+        print(f'[*] Compressing...')
         uncompressed = data.getvalue()
         # "Depending on the file, either first or last gives the best compression."
         buf = compress(uncompressed, blocksplittinglast=False)
         if zopfli is not None:
             maybe = compress(uncompressed, blocksplittinglast=True)
             if len(maybe) < len(buf):
-                print('[+] Block splitting last was better')
+                print(f'[+] Block splitting last was better')
                 buf = maybe
     else:
         buf = data.getbuffer()
@@ -221,13 +230,13 @@ def patch_binary(source, target, to_patch):
         f.write(buf)
 
 if __name__ == '__main__':
-    for source in argv[1:]:
+    if len(argv) == 2:
+        source = argv[1]
         print(f'Source: {source}')
         source_path = Path(source).absolute()
         data = _read_as_bytesio(source_path)
         x = ESPImage(data)
-'''
-    if len(argv) > 2:
+    elif len(argv) > 2:
         source, target = argv[1], argv[2]
         to_patch = {}
         for patch in argv[3:]:
@@ -236,6 +245,7 @@ if __name__ == '__main__':
 
         #print(source, target, to_patch)
         patch_binary(source, target, to_patch)
+'''
     elif len(argv) == 2:
         with open(argv[1]) as f:
             for base, module, template in map(lambda l: l.rstrip().split('\t'), f):
